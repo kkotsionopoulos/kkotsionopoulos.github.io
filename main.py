450efe90255fd7fd446479d8c53fb081fd285bae
kkotsionopoulos <k.kotsionopoulos@gmail.com>
2026-05-27 19:38:17 +0300

    ΝΕΟ6

--- main.py
+++ main.py
@@ -2,21 +2,15 @@ import os
 import requests
 import datetime
 
-# Ρύθμιση API KEY
+# Ρύθμιση API
 WEATHER_API_KEY = os.environ.get("WEATHER_API_KEY")
 
 def get_weather():
-    # Τρίπολη, Ελλάδα
     url = f"http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q=Tripoli,Greece"
     response = requests.get(url).json()
-    
     if 'current' not in response:
-        print(f"Σφάλμα API: {response}")
         return None, None
-        
-    temp = response['current']['temp_c']
-    humidity = response['current']['humidity']
-    return temp, humidity
+    return response['current']['temp_c'], response['current']['humidity']
 
 def calculate_risk(temp, humidity):
     if humidity > 75 and 15 <= temp <= 25:
@@ -31,13 +25,17 @@ temp, humidity = get_weather()
 
 if temp is not None:
     risk = calculate_risk(temp, humidity)
-    # Προσθήκη ώρας για να αλλάζει πάντα το αρχείο και να γίνεται push
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     content = f"Τελευταία ενημέρωση: {now}\nΘερμοκρασία: {temp}°C\nΥγρασία: {humidity}%\nΚατάσταση: {risk}"
     
+    # Γράψιμο στο αρχείο με ασφάλεια
     with open("result.txt", "w", encoding="utf-8") as f:
         f.write(content)
-    print("Το αρχείο result.txt ενημερώθηκε με:")
+        f.flush()
+        os.fsync(f.fileno())
+    
+    # Εκτύπωση στο log για να δούμε τι έγινε
+    print("Περιεχόμενο που γράφτηκε:")
     print(content)
 else:
     print("Αποτυχία λήψης δεδομένων.")
